64c8d3a24db0d0747f63a9b867b05d6186615682
Fleex <diego.ven80@gmail.com>
2022-11-22 00:18:13 -0600

    Actualizacion de los PDFs1

--- src/components/ContainerPDF.jsx
+++ src/components/ContainerPDF.jsx
@@ -60,16 +60,6 @@ const ContainerPDF = ({ mostrar }) => {
             <div style={{ padding: "0px 2px", marginLeft: "auto" }}>
               <EnterFullScreen />
             </div>
-           
-            {/* <div style={{ padding: "0px 5px" }}>
-              <a
-                href={PDF}
-                download
-                className="p-1 rounded-md border-solid border-1 border-gray-500 hover:bg-orange-600"
-              >
-                Descargar
-              </a> */}
-            {/* </div> */}
           </div>
         );
       }}
@@ -81,49 +71,13 @@ const ContainerPDF = ({ mostrar }) => {
     renderToolbar,
   });
 
-  // const [visualizarPDF, setVisualizarPDF] = useState(true);
-
-  // useEffect(() => {
-  //   if (
-  //     navigator.userAgent.match(/Android/i) ||
-  //     navigator.userAgent.match(/webOS/i) ||
-  //     navigator.userAgent.match(/iPhone/i) ||
-  //     navigator.userAgent.match(/iPad/i) ||
-  //     navigator.userAgent.match(/iPod/i) ||
-  //     navigator.userAgent.match(/BlackBerry/i) ||
-  //     navigator.userAgent.match(/Windows Phone/i)
-  //   ) {
-  //     setVisualizarPDF(false);
-  //   }
-  // }, []);
-
   return (
     <>
-      {/* {visualizarPDF ? ( */}
       <main className="m-auto md:w-1000 h-screen mt-14 md:mt-1">
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.0.279/build/pdf.worker.min.js">
           <Viewer plugins={[defaultLayoutPluginInstance]} fileUrl={PDF} />
         </Worker>
       </main>
-      {/* // ) : (
-      //   <main className="flex dark:bg-gray-600 dark:text-white m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl justify-center opacity-80">
-      //     <div className="p-5 bg-slate-100 dark:bg-gray-500 rounded-2xl">
-      //       <p className="pb-5">
-      //         <strong>
-      //           Lo sentimos pero no se puede ver la Revista en dispositivos
-      //           móviles y tablets.
-      //         </strong>
-      //       </p>
-      //       <a 
-      //         href={PDF}
-      //         download
-      //         className="mt-10 bg-orange-500 p-2 rounded-md"
-      //       >
-      //         Descargar Revista
-      //       </a>
-      //     </div>
-      //   </main>
-      //)} */}
     </>
   );
 };
